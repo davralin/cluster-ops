067ddbcb3f89a53f0fc25b4f89dd283dbac565ac
davralin <gpg@davralin.work>
2024-04-18 11:56:10 +0200

    fix(tube-archivist-meta): figured out why that didn't work...

--- meta/update-ansible-tags-from-renovate.sh
+++ meta/update-ansible-tags-from-renovate.sh
@@ -12,4 +12,4 @@ TUBEARCHIVIST_TAG=$(grep tag cluster/apps/tubearchivist/helm-release.yaml | cut 
 echo $TUBEARCHIVIST_TAG
 ANSIBLE_TUBEARCHIVIST_TAG=$(grep tubearchivist_compose_version ansible/roles/tubearchivist-compose/defaults/main.yml | cut -f2 -d'"')
 echo $ANSIBLE_TUBEARCHIVIST_TAG
-#sed 's/'$ANSIBLE_TUBEARCHIVIST_TAG'/'$TUBEARCHIVIST_TAG'/g' -i ansible/roles/tubearchivist-compose/defaults/main.yml
+sed 's/'$ANSIBLE_TUBEARCHIVIST_TAG'/'$TUBEARCHIVIST_TAG'/g' -i ansible/roles/tubearchivist-compose/defaults/main.yml
