b2746e3e46ae7a30598777d37b8fa62203ac1e85
davralin <gpg@davralin.work>
2024-06-01 09:13:31 +0200

    Update meta

--- meta/update-ansible-tags-from-renovate.sh
+++ meta/update-ansible-tags-from-renovate.sh
@@ -2,6 +2,6 @@
 # TubeArchivist
 TUBEARCHIVIST_TAG=$(grep tag cluster/apps/tubearchivist/helm-release.yaml | cut -f2 -d':' | sed 's/ //g')
 echo $TUBEARCHIVIST_TAG
-ANSIBLE_TUBEARCHIVIST_TAG=$(grep tubearchivist_compose_version ansible/roles/tubearchivist-compose/defaults/main.yml | cut -f2 -d'"')
+ANSIBLE_TUBEARCHIVIST_TAG=$(grep tubearchivist_compose_version ansible/roles/tubearchivist_compose/defaults/main.yml | cut -f2 -d'"')
 echo $ANSIBLE_TUBEARCHIVIST_TAG
-sed 's/'$ANSIBLE_TUBEARCHIVIST_TAG'/'$TUBEARCHIVIST_TAG'/g' -i ansible/roles/tubearchivist-compose/defaults/main.yml
+sed 's/'$ANSIBLE_TUBEARCHIVIST_TAG'/'$TUBEARCHIVIST_TAG'/g' -i ansible/roles/tubearchivist_compose/defaults/main.yml
