b2bd9a7b3e3b6a01a740512e2b500e8c87d770ae
davralin <gpg@davralin.work>
2024-06-02 09:26:16 +0200

    Let renovate handle it instead

--- meta/update-ansible-tags-from-renovate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# TubeArchivist
-TUBEARCHIVIST_TAG=$(grep tag cluster/apps/tubearchivist/helm-release.yaml | cut -f2 -d':' | sed 's/ //g')
-echo $TUBEARCHIVIST_TAG
-ANSIBLE_TUBEARCHIVIST_TAG=$(grep tubearchivist_compose_version ansible/roles/tubearchivist_compose/defaults/main.yml | cut -f2 -d'"')
-echo $ANSIBLE_TUBEARCHIVIST_TAG
-sed 's/'$ANSIBLE_TUBEARCHIVIST_TAG'/'$TUBEARCHIVIST_TAG'/g' -i ansible/roles/tubearchivist_compose/defaults/main.yml
