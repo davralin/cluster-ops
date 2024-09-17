01d6d57aa5d7d1d0010fbe8897462bca470c869f
davralin <gpg@davralin.work>
2024-09-17 15:37:31 +0200

    Update immich based on ansible-tags

---
+++ meta/immich-update-tags-from-ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get current tag
+IMMICH_TAG=$(grep immich-app ansible/roles/immich_compose/templates/docker-compose.yml | grep ghcr.io | cut -f3 -d':' | sort | uniq)
+echo $IMMICH_TAG
+
+# Get set tag
+HELM_TAG=$(grep tag cluster/apps/nextcloud/immich/helm-release.yaml  | cut -f2 -d'"')
+echo $HELM_TAG
+
+# Update with new tag
+sed 's/'$HELM_TAG'/'$IMMICH_TAG'/g' -i cluster/apps/nextcloud/immich/helm-release.yaml
