bfce13ebac72a8e9db65fea3e99573201944cc1b
davralin <gpg@davralin.work>
2025-04-12 22:27:33 +0200

    Adjustments for owntracks

---
+++ kubernetes/apps/owntracks/frontend-config.js
@@ -0,0 +1,7 @@
+// Here you can overwrite the default configuration values
+window.owntracks = window.owntracks || {};
+window.owntracks.config = {
+  api: {
+    baseUrl: "https://owntracks.${SECRET_DEFAULT_DOMAIN}",
+  },
+};
