e1efb37a476cc0feb5888f6c20b387499a774ef6
davralin <gpg@davralin.work>
2025-04-24 08:51:12 +0200

    Switch immich to using defaults in ansible-role

--- meta/immich-update-tags-from-ansible.sh
+++ meta/immich-update-tags-from-ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Get current tag
-IMMICH_TAG=$(grep immich-app ansible/roles/immich_compose/templates/docker-compose.yml | grep ghcr.io | cut -f3 -d':' | sort | uniq)
+IMMICH_TAG=$(grep immich_version ansible/roles/immich_compose/defaults/main.yml | cut -f2 -d' ')
 echo $IMMICH_TAG
 
 # Get set tag
-HELM_TAG=$(grep tag kubernetes/apps/nextcloud/immich/helm-release.yaml | cut -f2 -d'"')
+HELM_TAG=$(grep tag kubernetes/apps/nextcloud/immich/helm-release.yaml | cut -f2 -d':' | cut -f2 -d' ')
 echo $HELM_TAG
 
 # Update with new tag
