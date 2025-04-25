30a123a241ed809709ff225d46f761fc48a45801
davralin <gpg@davralin.work>
2025-04-26 01:46:46 +0200

    feat(ansible-certbot): add reload of haproxy to post-hook

--- ansible/roles/certbot/files/haproxy.sh
+++ ansible/roles/certbot/files/haproxy.sh
@@ -3,3 +3,5 @@ cd /etc/letsencrypt/live
 for i in $(find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
 	cat $i/fullchain.pem $i/privkey.pem > "/etc/haproxy/$i.pem"
 done
+
+/usr/bin/systemctl reload haproxy
