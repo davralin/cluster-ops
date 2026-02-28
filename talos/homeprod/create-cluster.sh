8da7d0dfd1d6ade4e6baee3129ce32e9b05e3971
davralin <gpg@davralin.work>
2026-02-28 15:23:32 +0100

    Update conf, ensure tuppr works

--- talos/homeprod/create-cluster.sh
+++ talos/homeprod/create-cluster.sh
@@ -3,7 +3,7 @@ talosctl gen secrets
 talosctl gen config --with-secrets secrets.yaml \
   --with-examples=false --with-docs=false \
   --config-patch @all-patch.yaml --config-patch-control-plane @cp-patch.yaml \
-  --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.8.1 \
+  --install-image factory.talos.dev/installer/ee827ee541fb06e742e6d183f4e3c1a62a2d63d945cbbd0b1e76da9221af9d27:v1.12.4 \
   HomeProd https://homeprod.internal:6443
-#  --install-image factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.8.1 \ # QEMU
-#  --install-image factory.talos.dev/installer/4b3cd373a192c8469e859b7a0cfbed3ecc3577c4a2d346a37b0aeff9cd17cdb0:v1.8.1 \ # Intel
+#  --install-image factory.talos.dev/installer/ee827ee541fb06e742e6d183f4e3c1a62a2d63d945cbbd0b1e76da9221af9d27:v1.12.4 \ # QEMU
+#  --install-image factory.talos.dev/installer/ee827ee541fb06e742e6d183f4e3c1a62a2d63d945cbbd0b1e76da9221af9d27:v1.12.4 \ # Intel
