d9e3ea6148d1da3f0be6320814c9ab16ca711f23
davralin <gpg@davralin.work>
2026-06-03 13:10:04 +0200

    Remove owntracks, I have switched to dawarich, Closes #6895

--- kubernetes/apps/owntracks/frontend-config.js
+++
@@ -1,7 +0,0 @@
-// Here you can overwrite the default configuration values
-window.owntracks = window.owntracks || {};
-window.owntracks.config = {
-  api: {
-    baseUrl: "https://owntracks.${SECRET_DEFAULT_DOMAIN}",
-  },
-};
